38a5bde8934b6ce9ca98bfdb9a2bd47f85b89dd3
moniramoni <moniinfo60@gmail.com>
2021-09-13 08:40:51 +0600

    ratings add

--- js/app.js
+++ js/app.js
@@ -17,7 +17,8 @@ const showProducts = (products) => {
         <img class="product-image" src=${image}></img>
       </div>
       <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
+      <p><strong>Category:</strong> <i>${product.category}</i></p>
+      <p><strong>Rate:</strong> ${product.rating.rate} <strong>Count:</strong> ${product.rating.count}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
@@ -76,6 +77,8 @@ const updateTotal = () => {
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal;
+  return grandTotal;
 };
+updateTotal();
 
 loadProducts();
